e3920a599c9eb8f82304ab9142e83f12ecd153f0
Dani <Dani@MSI-GL62M-7RD.mshome.net>
2017-12-08 17:17:40 +0000

    Kata5p1 versión 1

---
+++ src/kata5p1/Kata5p1.java
@@ -0,0 +1,41 @@
+package kata5p1;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class Kata5p1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws ClassNotFoundException, SQLException {
+
+        Class.forName("org.sqlite.JDBC");
+        Connection connection = DriverManager.getConnection("jdbc:sqlite:.\\kata5.db");
+
+        Statement statement = connection.createStatement();
+        String query = "Select * from people";
+        ResultSet resultset = statement.executeQuery(query);
+
+        ResultSetMetaData resultSetMetaData = resultset.getMetaData();
+
+        System.out.println(resultSetMetaData.getColumnName(1) + "\t"
+                + resultSetMetaData.getColumnName(2) + "\t\t"
+                + resultSetMetaData.getColumnName(3) + "\t"
+                + resultSetMetaData.getColumnName(4) + "\t");
+        while (resultset.next()) {
+            System.out.println(resultset.getInt(1) + "\t"
+                    + resultset.getString(2) + "\t\t"
+                    + resultset.getString(3) + "\t"
+                    + resultset.getString(4));
+        }
+
+        resultset.close();
+        statement.close();
+        connection.close();
+    }
+}
